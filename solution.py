63a933a046706b9f4ca6ccbb7491afb09e9d98d4
Nikolay Serebryakov <116488084+kolyasrbv@users.noreply.github.com>
2023-04-06 14:42:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 433193277
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    x = array.mean() / 86 
+    return x.mean()
